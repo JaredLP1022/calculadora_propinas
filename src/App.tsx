8a46d8937a2bb5942e9c45c920ea83c3c540eba0
JaredLP1022 <JaredPoseros@yahoo.com>
2024-08-19 23:57:55 -0600

    Se cambio el nombre del proyecto en el index.html y se creo el header de la pagina.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ function App() {
 
   return (
     <>
-      
+      <header className=" bg-teal-400 m-5">
+        <h1 className=" text-center text-4xl font-black"> Calculadora de Propinas y Consumo </h1>
+      </header>
     </>
   )
 }
