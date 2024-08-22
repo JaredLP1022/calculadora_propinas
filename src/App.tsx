bbd50e08df8e60ec4904bd757ffe22285f066118
JaredLP1022 <JaredPoseros@yahoo.com>
2024-08-21 23:42:55 -0600

    Actualizacion de app.tsx y creación de los items del menú

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,16 @@ function App() {
       </header>
 
       <main className="max-w-7xl mx-auto mt-3 grid md:grid-cols-2">
-        <div>
-          <h2> Menú</h2>
-          {menuItems.map(item => (
+        <div className="p-5">
+          <h2 className=" text-4xl font-black"> Menú</h2>
+         <div className="space-y-3 mt-5" >
+         {menuItems.map(item => (
             <MenuItem 
               key={item.id}
+              item = {item}
             />
           ))}
+         </div>
         </div>
         <div>
           <h2>Consumo</h2>
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,7 +1,16 @@
+import type { MenuItem } from "../types"
 
+type MneuItemProps = {
+    item: MenuItem
+}
 
-export default function MenuItem() {
-  return (
-    <div>MenuItem</div>
-  )
+export default function MenuItem({ item }: MneuItemProps) {
+    return (
+        <button
+            className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
+        >
+            <p>{item.name}</p>
+            <p className="font-black"> ${item.price} </p>
+        </button>
+    )
 }
